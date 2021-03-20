acf0713d1f74ced75c2ac26d71bbb359f6359b79
菊地祐貴 <kikuchiyuuki@kikuchiyuukinoMacBook-Air.local>
2021-03-20 18:22:44 +0900

    add title ButtonUI

--- lib/Camera.dart
+++ lib/Camera.dart
@@ -8,12 +8,25 @@ class ImagePickerView extends StatefulWidget {
     return Camera();
   }
 }
+
 //camera
-class Camera extends State {
+class Camera extends State<ImagePickerView> {
   File imageFile;
+  //タイトル
+  String ImageTitle = "タイトル名:";
+  // 入力された内容を保持するコントローラ
+  final inputController = TextEditingController();
+  //入力された時の処理
+  void setText (String s){
+    setState(() {
+      ImageTitle = s;
+    });
+  }
+  // 表示用の変数
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return Container(
         persistentFooterButtons: <Widget>[
           RaisedButton(
             child: Text(
@@ -33,8 +46,123 @@ class Camera extends State {
           ),
 
         ],
+        decoration: BoxDecoration(
+          // 枠線
+          border: Border.all(color: Colors.blue, width: 2),
+          // 角丸
+          borderRadius: BorderRadius.circular(8),
+        ),
 
-        body:  Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.all(30),
+              child: TextField(
+                decoration: InputDecoration(
+                  //Focusしていないとき
+                  enabledBorder: new OutlineInputBorder(
+                    borderRadius: new BorderRadius.circular(25.0),
+                    borderSide: BorderSide(
+                      color: Colors.black,
+                    ),
+
+                  ),
+                  //Focusしているとき
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: new BorderRadius.circular(25.0),
+                    borderSide: BorderSide(
+                      color: Colors.blue,
+                      width: 0.5,
+                    ),
+                  ),
+                  hintText: 'タイトル入力',
+                  contentPadding: EdgeInsets.fromLTRB(12,12,12,12),
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      Icons.check,
+                      color: Colors.black,
+                    ),
+
+                    onPressed: () {
+                      setText(inputController.text);
+
+                    },
+                  ),
+                ),
+              ),
+            ),
+
+
+            Text(ImageTitle),
+
+
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+
+                ButtonTheme(
+                  minWidth: 200.0,
+                  height: 60.0,
+                  child: RaisedButton.icon(
+                    icon: const Icon(
+                      Icons.camera_alt,
+                      color: Colors.white,
+                    ),
+                    label: const Text('カメラ起動'),
+                    onPressed: () {
+                      _getImageFromDevice(ImageSource.camera);
+                    },
+                    color: Colors.lightBlue,
+                    shape: const StadiumBorder(
+                      side: BorderSide(color: Colors.green),
+                    ),
+                    textColor: Colors.white,
+                  ),
+                ) ,
+                ButtonTheme(
+                  minWidth: 200.0,
+                  height: 60.0,
+                  child: RaisedButton.icon(
+                    icon: const Icon(
+                      Icons.folder_open,
+                      color: Colors.white,
+                    ),
+                    label: const Text('アルバムから取得'),
+                    onPressed: () {
+                      _getImageFromDevice(ImageSource.gallery);
+                    },
+                    color: Colors.lightBlue,
+                    shape: const StadiumBorder(
+                      side: BorderSide(color: Colors.green),
+                    ),
+                    textColor: Colors.white,
+                  ),
+                ),
+
+
+              ],
+            ),
+            (imageFile == null)
+                ? Icon(Icons.no_sim)
+                : Image.file(
+              imageFile,
+              height: 300.0,
+              width: 300.0,
+            ),
+            TextButton(
+              onPressed: () {
+                _deleteImage();
+              },
+              child: Text('画像の消去'),
+            ),
+
+          ],
+        ),
+
+
+        /*body:  Container(
               decoration: BoxDecoration(
                 // 枠線
                 border: Border.all(color: Colors.blue, width: 2),
@@ -45,39 +173,47 @@ class Camera extends State {
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: <Widget>[
-                  TextField(
-                   style: new TextStyle(
-                  fontSize: 40.0,
-                  height: 2.0,
-                  color: Colors.black
-                  ),
-                    decoration: InputDecoration(
-                      //Focusしていないとき
-                      enabledBorder: new OutlineInputBorder(
-                        borderRadius: new BorderRadius.circular(25.0),
-                        borderSide: BorderSide(
-                          color: Colors.black,
+                  Container(
+                    margin: EdgeInsets.all(30),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        //Focusしていないとき
+                        enabledBorder: new OutlineInputBorder(
+                          borderRadius: new BorderRadius.circular(25.0),
+                          borderSide: BorderSide(
+                            color: Colors.black,
+                          ),
+
                         ),
-                      ),
-                      //Focusしているとき
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: new BorderRadius.circular(25.0),
-                        borderSide: BorderSide(
-                          color: Colors.blue,
-                          width: 0.5,
+                        //Focusしているとき
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: new BorderRadius.circular(25.0),
+                          borderSide: BorderSide(
+                            color: Colors.blue,
+                            width: 0.5,
+                          ),
                         ),
-                      ),
-                      hintText: 'タイトル入力',
-                      contentPadding: EdgeInsets.all(10.0),
-                      suffixIcon: IconButton(
-                        icon: Icon(
-                          Icons.check,
-                          color: Colors.black,
+                        hintText: 'タイトル入力',
+                        contentPadding: EdgeInsets.fromLTRB(12,12,12,12),
+                        suffixIcon: IconButton(
+                          icon: Icon(
+                            Icons.check,
+                            color: Colors.black,
+                          ),
+
+                          onPressed: () {
+                            setText(inputController.text);
+
+                          },
                         ),
-                        onPressed: () {},
                       ),
                     ),
                   ),
+
+
+                  Text(ImageTitle),
+
+
 
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -123,18 +259,6 @@ class Camera extends State {
                    ),
 
 
-
-
-                      /*RaisedButton(
-
-                        child: const Text('Button'),
-                        color: Colors.white,
-                        shape: const StadiumBorder(
-                          side: BorderSide(color: Colors.green),
-                        ),
-                        onPressed: () {},
-                      ),*/
-
                     ],
                   ),
                       (imageFile == null)
@@ -150,38 +274,10 @@ class Camera extends State {
                         },
                         child: Text('画像の消去'),
                       ),
-                  /*TextField(
-                    decoration: InputDecoration(
-                      //Focusしていないとき
-                      enabledBorder: new OutlineInputBorder(
-                        borderRadius: new BorderRadius.circular(25.0),
-                        borderSide: BorderSide(
-                          color: Colors.black,
-                        ),
-                      ),
-                      //Focusしているとき
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: new BorderRadius.circular(25.0),
-                        borderSide: BorderSide(
-                          color: Colors.blue,
-                          width: 3.0,
-                        ),
-                      ),
-                      hintText: 'タイトル入力',
-                      contentPadding: EdgeInsets.all(16.0),
-                      suffixIcon: IconButton(
-                        icon: Icon(
-                          Icons.add,
-                          color: Colors.black,
-                        ),
-                        onPressed: () {},
-                      ),
-                    ),
-                  ),*/
 
                 ],
               ),
-            )
+            )*/
 
 
 
